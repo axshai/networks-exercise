7473b48b9b5602937463a524b47bc7ddc908ab6a
Shai Axelrod <Saxelrod@g.jct.ac.il>
2021-09-24 15:46:57 +0300

    ZanValzan

---
+++ zanValzan.py
@@ -0,0 +1,9 @@
+num=input("Please enter a 5 digit number\n")
+print("You entered the number: {}".format(num))
+sum=0
+print("The digits of this number are: ", end="")
+for i in num[:-1]:
+    sum+=int(i)
+    print(i+",", end="")
+print(num[-1])
+print("The sum of the digits is: {}".format(sum+int(num[-1])), end="")
