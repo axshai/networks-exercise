cb3d9533d5778bc707a61dd9b0900d39ec24c6d3
Shai Axelrod <Saxelrod@g.jct.ac.il>
2021-09-30 16:23:54 +0300

    ZanValzan

--- zanValzan.py
+++ zanValzan.py
@@ -1,9 +1,13 @@
-num = input("Please enter a 5 digit number\n")
-print("You entered the number: {}".format(num))
-sum = 0
-print("The digits of this number are: ", end="")
-for i in num[:-1]:
-    sum += int(i)
-    print(i + ",", end="")
-print(num[-1])
-print("The sum of the digits is: {}".format(sum + int(num[-1])), end="")
+def main():
+    num = input("Please enter a 5 digit number\n")
+    print("You entered the number: {}".format(num))
+    sum = 0
+    print("The digits of this number are: ", end="")
+    for i in num[:-1]:
+        sum += int(i)
+        print(i + ",", end="")
+    print(num[-1])
+    print("The sum of the digits is: {}".format(sum + int(num[-1])), end="")
+
+if __name__ == "__main__":
+    main()
